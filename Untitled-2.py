c10e7cd6deda826ebcc1f7213b3396a064a7074c
Saleh Almarri (s5724906) <s5724906@bournemouth.ac.uk>
2024-10-15 12:38:49 +0100

    Initial message

---
+++ Untitled-2.py
@@ -0,0 +1,15 @@
+print("hello")
+print(44+66)
+anmals=["dog","cat","rabit"]
+print(anmals[2])
+import time
+
+# Save timestamp
+start = time.time()
+
+print(23*2.3)
+
+# Save timestamp
+end = time.time()
+
+print(end - start)
